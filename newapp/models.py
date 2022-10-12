f8a9115dbdf3f873e78a411a5c8d6132295be4a0
Gautam010 <85187336+Gautam010@users.noreply.github.com>
2022-10-12 18:22:51 +0530

    added image field to post model

--- newapp/models.py
+++ newapp/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Post(models.Model):
     title=models.CharField(max_length=20)
     content=models.TextField()
+    image=models.FileField(upload_to="pic",null=True,blank=True)
+    created_on=models.DateTimeField(auto_now_add=True,null=True)
 
     def __str__(self):
         return self.title
-# Create your models here.
+
